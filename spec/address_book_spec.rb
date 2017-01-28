f93c8f843d5ffebb0b0f9a3d87d3ea8aaea7bc3e
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-01-28 14:12:58 -0600

    assignment 7 complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -87,4 +87,37 @@ require_relative '../models/address_book'
            check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
          end
        end
-end
+
+    describe "#import_entries_from_csv_2" do
+          it "imports the correct number of entries" do
+         # #3
+            book.import_from_csv("entries_2.csv")
+            book_size = book.entries.size
+
+         # Check the size of the entries in AddressBook
+            expect(book_size).to eq 3
+          end
+
+          it "imports the 1st entry" do
+             book.import_from_csv("entries_2.csv")
+         # Check the first entry
+             entry_one = book.entries[0]
+             check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+           end
+
+           it "imports the 2nd entry" do
+             book.import_from_csv("entries_2.csv")
+         # Check the second entry
+             entry_two = book.entries[1]
+             check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+           end
+
+           it "imports the 3rd entry" do
+             book.import_from_csv("entries_2.csv")
+         # Check the third entry
+             entry_three = book.entries[2]
+             check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+           end
+
+         end
+  end
