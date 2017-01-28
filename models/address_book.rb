0c927ecf476e95b723f8c659e3d0ec00e42d5023
Walter Baggett <walterbaggett@legalshieldcorp.com>
2017-01-28 13:22:50 -0600

    checkpoint 7 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
    attr_reader :entries
@@ -19,5 +20,15 @@ class AddressBook
         end
         # #11
         entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # #8
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
     end
+end
